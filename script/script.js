ba12a32624256b140a558b8f8c7679c3c6d4cbfd
r1xez <slavk534226@gmail.com>
2024-09-29 11:50:27 +0300

    1

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
   $('.carousele').slick({
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true, // Відображення крапок для навігації
