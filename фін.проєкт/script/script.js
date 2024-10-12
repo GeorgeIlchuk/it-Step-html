b0f421e6c4487088cf00eadf5f38e4e8cac3f55c
George <georgiy.ilchuk2009@gmail.com>
2024-10-12 21:51:21 +0300

    Опис змін

--- фін.проєкт/script/script.js
+++ фін.проєкт/script/script.js
@@ -1,10 +1,9 @@
 
 $(document).ready(function(){
   $('.carousele').slick({
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
     dots: true, // Відображення крапок для навігації
     arrows: true, // Відображення стрілок
   });
